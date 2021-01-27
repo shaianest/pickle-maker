5c0fcba64d46602695ea1f754138e6a146745985
shaianest <yshaian.kiany1@gmail.com>
2021-01-27 13:35:17 +0330

    commited

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn angular
         </a>
       </header>
     </div>
